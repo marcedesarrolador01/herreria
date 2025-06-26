90e0b3956ce368bb1ac053a9392b9a03e7e71f50
marcelodesarrollador02 <herreriasoldaduraunio2025@gmail.com>
2025-06-26 02:29:35 -0300

    Agrego script para crear superusuario

---
+++ create_superuser.py
@@ -0,0 +1,15 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'herrero.settings')  # si tu settings está en herrero/settings.py
+django.setup()
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("alejandroperez", "admin@example.com", "herreria2025perez270625")
+    print("✅ Superusuario creado")
+else:
+    print("⚠️ El superusuario ya existe")
